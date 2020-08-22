ba8570d3824c6e79e449a0c28374b462f61fc8b7
Alan Chambers <ajmchambers@gmail.com>
2020-08-22 17:23:57 +0100

    added build target

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,11 +2,16 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'al-card',
   taskQueue: 'async',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@ajmchambers/al-card',
+      proxiesFile: '../al-card-react/src/components.ts'
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
